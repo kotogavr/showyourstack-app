2127dab40be436b8f5cb8d21811fbdc5acab1f4c
Ann <59127075+kotogavr@users.noreply.github.com>
2024-02-13 09:19:28 +0000

    Add harmonyPalette colors and update dependencies

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,10 @@
+import harmonyPalette from "@evilmartians/harmony/tailwind";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+    colors: harmonyPalette,
 		extend: {},
 	},
 	plugins: [],
